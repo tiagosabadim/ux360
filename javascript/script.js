9f6f8f469c6da1bf832fb332a06b929886c5ff76
Tiago Sabadim <tiagoappdesign@gmail.com>
2023-03-21 00:28:41 -0300

    commit mobile

--- javascript/script.js
+++ javascript/script.js
@@ -1,6 +1,19 @@
 const navbarMenu = document.querySelector(".navbar__menu");
 const navbarMenuIcon = document.querySelector(".navbar__menu-icon");
+const linksMenu = document.querySelectorAll('.navbar__menu li a');
 
 navbarMenuIcon.addEventListener("click", () => {
   navbarMenu.classList.toggle("navbar__menu--active");
 });
+
+function fecharMenu() {
+  navbarMenu.classList.remove("navbar__menu--active");
+}
+
+linksMenu.forEach(function(link) {
+  link.addEventListener('click', function() {
+    fecharMenu();
+  });
+});
+
+
